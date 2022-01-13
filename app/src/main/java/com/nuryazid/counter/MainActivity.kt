d5c7c7e90d3e896bbd4ac5adcd2c0f17196e8b51
Zidd <nuvyazid@gmail.com>
2022-01-13 11:45:54 +0700

    no-mvvm

--- app/src/main/java/com/nuryazid/counter/MainActivity.kt
+++ app/src/main/java/com/nuryazid/counter/MainActivity.kt
@@ -3,39 +3,27 @@ package com.nuryazid.counter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.ViewModelProvider
 import com.nuryazid.counter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    //region unused
-    /*private var counter = 0*/
-    //endregion
+    private var counter = 0
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
 
-        binding.lifecycleOwner = this
-
-        val viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-
-        binding.viewModel = viewModel
-
-        //region unused
-        /*binding.btnPlus.setOnClickListener {
+        binding.btnPlus.setOnClickListener {
             increaseCount()
         }
 
         binding.btnMinus.setOnClickListener {
             decreaseCount()
-        }*/
-        //endregion
+        }
     }
 
-    //region unused
-    /*private fun increaseCount() {
+    private fun increaseCount() {
         counter += 1
         binding.tvCount.text = "$counter"
     }
@@ -43,6 +31,5 @@ class MainActivity : AppCompatActivity() {
     private fun decreaseCount() {
         counter -= 1
         binding.tvCount.text = "$counter"
-    }*/
-    //endregion
+    }
 }
