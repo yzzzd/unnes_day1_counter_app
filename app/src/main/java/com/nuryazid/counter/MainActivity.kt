eb7fe39e0bdb712fce2eeb39d15ddda81a553949
Zidd <nuvyazid@gmail.com>
2022-01-13 11:47:08 +0700

    new project

--- app/src/main/java/com/nuryazid/counter/MainActivity.kt
+++ app/src/main/java/com/nuryazid/counter/MainActivity.kt
@@ -2,34 +2,10 @@ package com.nuryazid.counter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.databinding.DataBindingUtil
-import com.nuryazid.counter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
-    private var counter = 0
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
-
-        binding.btnPlus.setOnClickListener {
-            increaseCount()
-        }
-
-        binding.btnMinus.setOnClickListener {
-            decreaseCount()
-        }
-    }
-
-    private fun increaseCount() {
-        counter += 1
-        binding.tvCount.text = "$counter"
-    }
-
-    private fun decreaseCount() {
-        counter -= 1
-        binding.tvCount.text = "$counter"
+        setContentView(R.layout.activity_main)
     }
 }
